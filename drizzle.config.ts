d358f08fa06e7fd2de53388a61a9ad60d9e30941
Matheus Bronca <matheus.bronca@frncubo.com>
2025-09-23 11:13:20 -0300

    feat: add ci/cd

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   schema: ['./src/database/schema.ts', './src/**/schema.ts'],
   out: './drizzle',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: Number(process.env.POSTGRES_PORT) || 5444,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE || 'username',
   },
 });
